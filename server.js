1a1866a9e3fceb02ad2111dc03b048fd2bbb8975
⠛⠥⠍⠊⠇⠇⠕ <spx.kuba@gmail.com>
2026-06-08 01:11:15 +0200

    Complete section 26.3 - Template rendering with Handlebars

--- server.js
+++ server.js
@@ -1,40 +1,54 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-// Middleware adding res.show helper
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+// Configure template engine
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './views/layouts', defaultLayout: 'main' }));
+app.set('view engine', 'hbs');
 
 // Middleware for static files
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Middleware for user paths check (forbidden)
 app.use('/user', (req, res, next) => {
-  res.show('forbidden.html');
+  res.render('forbidden');
 });
 
 // Routes
 app.get('/', (req, res) => {
-  res.show('home.html');
+  res.render('index');
 });
 
 app.get('/home', (req, res) => {
-  res.show('home.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  // Use custom layout 'dark'
+  res.render('contact', { layout: 'dark' });
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 // 404 handler
 app.use((req, res) => {
-  res.status(404).show('404.html');
+  res.status(404).render('404');
 });
 
 app.listen(8000, () => {
