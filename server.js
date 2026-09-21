eefd6f5e001bcecd7a7e65f2eb3190d2715b4c8f
⠛⠥⠍⠊⠇⠇⠕ <spx.kuba@gmail.com>
2026-09-21 11:46:13 +0200

    26.2 Express routing basic server

--- server.js
+++ server.js
@@ -1,40 +1,29 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Middleware adding res.show helper
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
-// Middleware for static files
-app.use(express.static(path.join(__dirname, '/public')));
-
-// Middleware for user paths check (forbidden)
-app.use('/user', (req, res, next) => {
-  res.show('forbidden.html');
-});
-
-// Routes
 app.get('/', (req, res) => {
-  res.show('home.html');
-});
-
-app.get('/home', (req, res) => {
-  res.show('home.html');
+  res.send('<h1>My first server!</h1>');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.send('<h1>About</h1>');
 });
 
-// 404 handler
+app.get('/contact', (req, res) => {
+  res.send('<h1>Contact</h1>');
+});
+
+app.get('/info', (req, res) => {
+  res.send('<h1>Info</h1>');
+});
+
+app.get('/history', (req, res) => {
+  res.send('<h1>History</h1>');
+});
+
 app.use((req, res) => {
-  res.status(404).show('404.html');
+  res.status(404).send('<h1>404 Not Found...</h1>');
 });
 
 app.listen(8000, () => {
