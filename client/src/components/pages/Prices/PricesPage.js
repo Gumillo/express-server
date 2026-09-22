c34e4ed1291c648ea2c021d6fcd8ad3d1f1fcd99
⠛⠥⠍⠊⠇⠇⠕ <spx.kuba@gmail.com>
2026-09-22 23:07:30 +0200

    29.5 - Add request state handling (pending/error/success) to PricesPage

--- client/src/components/pages/Prices/PricesPage.js
+++ client/src/components/pages/Prices/PricesPage.js
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Alert, Container } from 'reactstrap';
-import { getConcerts, loadConcertsRequest } from '../../../redux/concertsRedux';
+import { Alert, Container, Progress } from 'reactstrap';
+import { getConcerts, getRequest, loadConcertsRequest } from '../../../redux/concertsRedux';
 
 const Prices = () => {
   const dispatch = useDispatch();
   const concerts = useSelector(getConcerts);
+  const request = useSelector(getRequest);
 
   useEffect(() => {
     dispatch(loadConcertsRequest());
@@ -39,7 +40,9 @@ const Prices = () => {
           Attention! <strong>Children under 4 can go freely with you without any other fee!</strong>
       </Alert>
 
-      {daysArray.map(({ day, price }) => (
+      {request.pending && <Progress animated color="primary" value={50} />}
+      {request.error && <Alert color="warning">{request.error}</Alert>}
+      {request.success && daysArray.map(({ day, price }) => (
         <div key={day}>
           <h2>Day {day === 1 ? 'one' : day === 2 ? 'Two' : day === 3 ? 'three' : day}</h2>
           <p>Price: {price}$</p>
